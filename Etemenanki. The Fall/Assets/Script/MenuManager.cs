dbb6fd3dc3c3b17ed3873976d99999157c137d61
Pirsona <LerouSanchez@yandex.ru>
2024-04-27 14:16:10 +0500

    Работа с переходами
    Настроил скрипт на переходы в разные части меню и добавил методы:
    -активация main menu и его деактивация;
    -выход из игры;

---
+++ Etemenanki. The Fall/Assets/Script/MenuManager.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MenuManager : MonoBehaviour
+{
+    public Button startGame;
+    public Button settingsGame;
+    public Button exitGame;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void Quit() // Метод выхода из игры
+    {
+        Application.Quit();
+        Debug.Log("You Quit");
+    }
+    public void DisableMainMenu() // Деактивирует кнопки главного меню
+    {
+       startGame.enabled = false;
+       settingsGame.enabled = false;
+       exitGame.enabled = false;   
+    }
+    public void ActiveMainMenu() // Активирует кнопки главного меню
+    {
+        startGame.enabled = true;
+        settingsGame.enabled = true;
+        exitGame.enabled = true;
+    }
+}
