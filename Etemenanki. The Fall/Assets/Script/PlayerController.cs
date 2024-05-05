3df741cd5893d9ed06a9a029f93709b94b948267
Pirsona <LerouSanchez@yandex.ru>
2024-05-05 09:19:35 +0500

    Доделал полностью контроллер персонажа
    Вставил готовый контроллер персонажа и добавил атаки

--- Etemenanki. The Fall/Assets/Script/PlayerController.cs
+++ Etemenanki. The Fall/Assets/Script/PlayerController.cs
@@ -1,65 +1,208 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
+    PlayerInput playerInput;
+    PlayerInput.MainActions input;
 
-    public float gravity = 9.8f;
-    public float jumpForce;
-    public float speed;
+    CharacterController controller;
+    Animator animator;
+    AudioSource audioSource;
 
-    private Vector3 _moveVector;
-    private float _fallVelocity = 0;
+    [Header("Controller")]
+    public float moveSpeed = 5;
+    public float gravity = -9.8f;
+    public float jumpHeight = 1.2f;
 
-    private CharacterController _characterController;
+    Vector3 _PlayerVelocity;
 
-    void Start()
-    {
-        _characterController = GetComponent<CharacterController>();
+    bool isGrounded;
+
+    [Header("Camera")]
+    public Camera cam;
+    public float sensitivity;
+
+    float xRotation = 0f;
+
+    void Awake()
+    { 
+        controller = GetComponent<CharacterController>();
+        animator = GetComponentInChildren<Animator>();
+        audioSource = GetComponent<AudioSource>();
+
+        playerInput = new PlayerInput();
+        input = playerInput.Main;
+        AssignInputs();
+
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
     }
 
     void Update()
     {
-        _moveVector = Vector3.zero;
+        isGrounded = controller.isGrounded;
 
-        if (Input.GetKey(KeyCode.W))
-        {
-            _moveVector += transform.forward;
-        }
+        // Repeat Inputs
+        if(input.Attack.IsPressed())
+        { Attack(); }
 
-        if (Input.GetKey(KeyCode.S))
-        {
-            _moveVector -= transform.forward;
-        }
+        SetAnimations();
+    }
 
-        if (Input.GetKey(KeyCode.D))
-        {
-            _moveVector += transform.right;
-        }
+    void FixedUpdate() 
+    { MoveInput(input.Movement.ReadValue<Vector2>()); }
 
-        if (Input.GetKey(KeyCode.A))
-        {
-            _moveVector -= transform.right;
-        }
+    void LateUpdate() 
+    { LookInput(input.Look.ReadValue<Vector2>()); }
 
-        if (Input.GetKeyDown(KeyCode.Space) && _characterController.isGrounded)
+    void MoveInput(Vector2 input)
+    {
+        Vector3 moveDirection = Vector3.zero;
+        moveDirection.x = input.x;
+        moveDirection.z = input.y;
+
+        controller.Move(transform.TransformDirection(moveDirection) * moveSpeed * Time.deltaTime);
+        _PlayerVelocity.y += gravity * Time.deltaTime;
+        if(isGrounded && _PlayerVelocity.y < 0)
+            _PlayerVelocity.y = -2f;
+        controller.Move(_PlayerVelocity * Time.deltaTime);
+    }
+
+    void LookInput(Vector3 input)
+    {
+        float mouseX = input.x;
+        float mouseY = input.y;
+
+        xRotation -= (mouseY * Time.deltaTime * sensitivity);
+        xRotation = Mathf.Clamp(xRotation, -80, 80);
+
+        cam.transform.localRotation = Quaternion.Euler(xRotation, 0, 0);
+
+        transform.Rotate(Vector3.up * (mouseX * Time.deltaTime * sensitivity));
+    }
+
+    void OnEnable() 
+    { input.Enable(); }
+
+    void OnDisable()
+    { input.Disable(); }
+
+    void Jump()
+    {
+        // Adds force to the player rigidbody to jump
+        if (isGrounded)
+            _PlayerVelocity.y = Mathf.Sqrt(jumpHeight * -3.0f * gravity);
+    }
+
+    void AssignInputs()
+    {
+        input.Jump.performed += ctx => Jump();
+        input.Attack.started += ctx => Attack();
+    }
+
+    // ---------- //
+    // ANIMATIONS //
+    // ---------- //
+
+    public const string IDLE = "Idle";
+    public const string WALK = "Walk";
+    public const string ATTACK1 = "Attack 1";
+    public const string ATTACK2 = "Attack 2";
+
+    string currentAnimationState;
+
+    public void ChangeAnimationState(string newState) 
+    {
+        // STOP THE SAME ANIMATION FROM INTERRUPTING WITH ITSELF //
+        if (currentAnimationState == newState) return;
+
+        // PLAY THE ANIMATION //
+        currentAnimationState = newState;
+        animator.CrossFadeInFixedTime(currentAnimationState, 0.2f);
+    }
+
+    void SetAnimations()
+    {
+        // If player is not attacking
+        if(!attacking)
         {
-            _fallVelocity = -jumpForce;
+            if(_PlayerVelocity.x == 0 &&_PlayerVelocity.z == 0)
+            { ChangeAnimationState(IDLE); }
+            else
+            { ChangeAnimationState(WALK); }
         }
     }
 
-    void FixedUpdate()
+    // ------------------- //
+    // ATTACKING BEHAVIOUR //
+    // ------------------- //
+
+    [Header("Attacking")]
+    public float attackDistance = 3f;
+    public float attackDelay = 0.4f;
+    public float attackSpeed = 1f;
+    public int attackDamage = 1;
+    public LayerMask attackLayer;
+
+    public GameObject hitEffect;
+    public AudioClip swordSwing;
+    public AudioClip hitSound;
+
+    bool attacking = false;
+    bool readyToAttack = true;
+    int attackCount;
+
+    public void Attack()
     {
-        _characterController.Move(_moveVector * speed * Time.fixedDeltaTime);
+        if(!readyToAttack || attacking) return;
 
-        _fallVelocity += gravity * Time.fixedDeltaTime;
+        readyToAttack = false;
+        attacking = true;
 
-        _characterController.Move(Vector3.down * _fallVelocity * Time.fixedDeltaTime);
+        Invoke(nameof(ResetAttack), attackSpeed);
+        Invoke(nameof(AttackRaycast), attackDelay);
 
-        if (_characterController.isGrounded)
+        audioSource.pitch = Random.Range(0.9f, 1.1f);
+        audioSource.PlayOneShot(swordSwing);
+
+        if(attackCount == 0)
         {
-            _fallVelocity = 0;
+            ChangeAnimationState(ATTACK1);
+            attackCount++;
         }
+        else
+        {
+            ChangeAnimationState(ATTACK2);
+            attackCount = 0;
+        }
+    }
+
+    void ResetAttack()
+    {
+        attacking = false;
+        readyToAttack = true;
+    }
+
+    void AttackRaycast()
+    {
+        if(Physics.Raycast(cam.transform.position, cam.transform.forward, out RaycastHit hit, attackDistance, attackLayer))
+        { 
+            HitTarget(hit.point);
+
+            if(hit.transform.TryGetComponent<Actor>(out Actor T))
+            { T.TakeDamage(attackDamage); }
+        } 
+    }
+
+    void HitTarget(Vector3 pos)
+    {
+        audioSource.pitch = 1;
+        audioSource.PlayOneShot(hitSound);
+
+        GameObject GO = Instantiate(hitEffect, pos, Quaternion.identity);
+        Destroy(GO, 20);
     }
 }
